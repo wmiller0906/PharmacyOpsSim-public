b469f0ef78fda10b26a0ffd7bbcddfc91330e412
William Miller <136769841+wmiller0906@users.noreply.github.com>
2025-05-29 23:02:50 -0400

    Created Note.h

---
+++ include/Note.h
@@ -0,0 +1,29 @@
+#ifndef NOTE_H
+#define NOTE_H
+
+#include <string>
+
+class User;
+
+class Note {
+    private:
+        std::string text;
+        std::string author;
+        std::string dateAndTime;
+        std::string digitalSignature;
+        
+    public:
+        // Constructor
+        Note();
+    
+        // Factory function
+        static Note create_note(const std::string& input, const User& user);
+        
+        // Accessor
+        std::string get_text() const;
+        std::string get_author() const;
+        std::string get_date() const;
+        std::string get_digital_signature() const;
+};
+
+#endif
