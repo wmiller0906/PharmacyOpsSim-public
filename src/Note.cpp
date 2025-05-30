e19597488355190af89943447b30e0952ff021c8
William Miller <136769841+wmiller0906@users.noreply.github.com>
2025-05-29 23:03:29 -0400

    Created Note.cpp

---
+++ src/Note.cpp
@@ -0,0 +1,50 @@
+#include "Note.h"
+
+#include "My_Utility.h"
+#include "User.h"
+
+// Constructor
+Note::Note() : 
+    text(""),
+    author(""),
+    dateAndTime(""),
+    digitalSignature("") {}
+    
+// Factory function
+Note Note::create_note(const std::string& input, const User& user) {
+    Note n;
+    
+    n.text = input;
+    n.author = user.get_initials();
+    n.dateAndTime = get_current_time();
+    n.digitalSignature = n.author + "@" + n.dateAndTime;
+    
+    append_log(n.digitalSignature);
+    return n;
+}
+
+// Accessor
+std::string Note::get_text() const {
+    return text;
+}
+
+std::string Note::get_author() const {
+    return author;
+}
+
+
+std::string Note::get_date() const {
+    if(dateAndTime.length() < 10) {
+        throw std::invalid_argument("dateAndTime string too short");
+    }
+    
+    std::string year = dateAndTime.substr(0, 4);
+    std::string month = dateAndTime.substr(5, 2);
+    std::string day = dateAndTime.substr(8, 2);
+    
+    return month + "/" + day + "/" + year;
+}
+
+std::string Note::get_digital_signature() const {
+    return digitalSignature;
+}
